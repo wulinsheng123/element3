ae053f8571a1a29e916750bb1132316ccc465b2d
670878721@qq126.com <i95966@126.com>
2021-01-25 21:00:12 +0800

    test: (test cases)

--- packages/element3/src/components/CarouselItem/src/CarouselItem.vue
+++ packages/element3/src/components/CarouselItem/src/CarouselItem.vue
@@ -1,5 +1,24 @@
 <template>
-  <div></div>
+  <div v-show="ready" class="el-carousel__item"></div>
 </template>
 
-<script></script>
+<script>
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      ready: false
+    })
+
+    const { ready } = toRefs(state)
+
+    const translateItem = () => (state.ready = true)
+
+    return {
+      ready,
+      translateItem
+    }
+  }
+})
+</script>
